80a2fca1478c77721f542befcce2bb37782f8d85
aminekhadraoui <miral.97@bk.ru>
2022-02-08 02:36:35 +0100

    angular project

--- shared/mock-data/catalogue-produits.ts
+++ shared/mock-data/catalogue-produits.ts
@@ -28,6 +28,30 @@ export const CATALOGUE: learn[] = [
     id: 'C1',
     image: 'assets/4.png',
     description: 'Vue js from scratch',
+  },{
+    nom: 'laravel',
+    prix: '450dt',
+    id: 'backEnd',
+    image: 'assets/laravel.png',
+    description: 'Learn to master Laravel to make advanced applications like the real CMS app we build on this course',
+  },{
+    nom: 'node',
+    prix: '450dt',
+    id: 'backEnd',
+    image: 'assets/node.png',
+    description: 'Learn Node.js by building real-world applications with Node JS, Express, MongoDB, Jest, and more!',
+  },{
+    nom: 'java',
+    prix: '450dt',
+    id: 'backEnd',
+    image: 'assets/java.png',
+    description: 'Learn Java In This Course And Become a Computer Programmer. Obtain valuable Core Java Skills And Java Certification',
+  },{
+    nom: 'delphi',
+    prix: '450dt',
+    id: 'backEnd',
+    image: 'assets/delphi.png',
+    description: 'Master programming on Windows or Mac OS X with Lazarus & Free Pascal or Delphi',
   },
   
 ];
